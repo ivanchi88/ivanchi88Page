a2e1e9029ff3c6e4db884c0cede06e8c82ad8cdc
ivanchi88 <ivanmotos_8@hotmail.es>
2019-05-16 23:48:23 +0200

    Fixed env port

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var app = express();
+const PORT = process.env.PORT || 5000
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
 app.listen(proccess.env.PORT, function () {
-  console.log(`Example app listening on port ${proccess.env.PORT}!`);
+  console.log(`Example app listening on port ${PORT}!`);
 });
