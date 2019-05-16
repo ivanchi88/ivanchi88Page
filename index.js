c68becd18ac781571dd27c60ebedda2dc667a8ac
ivanchi88 <ivanmotos_8@hotmail.es>
2019-05-17 00:10:19 +0200

    fixed page

--- index.js
+++ index.js
@@ -2,10 +2,14 @@ var express = require('express');
 var app = express();
 const PORT = process.env.PORT || 5000
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.use('/static', express.static(__dirname + '/static'));
+
+// viewed at http://localhost:8080
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/static/html/index.html');
 });
 
-app.listen(proccess.env.PORT, function () {
+app.listen(PORT, function () {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
