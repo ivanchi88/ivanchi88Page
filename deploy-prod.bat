d1e3adbc039c4aa7d23999f5b3d71a37027c3e2c
ivanchi88 <ivanmotos_8@hotmail.es>
2019-06-23 00:11:02 +0200

    deploy

--- deploy-prod.bat
+++ deploy-prod.bat
@@ -3,3 +3,9 @@ call npm run deploy
 call git add .
 call git commit -m "deploy" 
 call git push heroku master 
+
+call git push origin master
+
+call git checkout -b "deploy"
+call git push origin master
+call git checkout master
