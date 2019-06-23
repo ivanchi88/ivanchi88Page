ea878f6a7e708fb67728c1b92cc3b11da4570003
ivanchi88 <ivanmotos_8@hotmail.es>
2019-06-23 19:51:27 +0200

    deploy

--- front/src/app/Pages/spriteStacker/spriteStacker.component.ts
+++ front/src/app/Pages/spriteStacker/spriteStacker.component.ts
@@ -1,5 +1,4 @@
 import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core'; 
-import { CONTEXT } from '@angular/core/src/render3/interfaces/view';
 
 @Component({
     selector: 'app-spriteStacker-page',
@@ -8,12 +7,16 @@ import { CONTEXT } from '@angular/core/src/render3/interfaces/view';
   })
   export class SpriteStacker implements AfterViewInit  {
     @ViewChild('colorsCanvas') colorsCanvas: ElementRef;
-    public context: CanvasRenderingContext2D;
+    public colorsContext: CanvasRenderingContext2D;
+
+    @ViewChild('paintingsCanvas') paintingCanvas: ElementRef;
+    public paintingContext: CanvasRenderingContext2D;
 
     colors : string [] = ["#003399", "#009933", "#ff3300", "#660066", "#ffff00", "#ff6600", "#993300", "#99ccff", "#ffffff", "#000000"];
 
     ngAfterViewInit(): void {
-      this.context = (<HTMLCanvasElement>this.colorsCanvas.nativeElement).getContext('2d');
+      this.colorsContext = (<HTMLCanvasElement>this.colorsCanvas.nativeElement).getContext('2d');
+      this.paintingContext = (<HTMLCanvasElement>this.paintingCanvas.nativeElement).getContext('2d');
       
       let width =  this.colorsCanvas.nativeElement.width;
       let height = this.colorsCanvas.nativeElement.height;
@@ -23,11 +26,11 @@ import { CONTEXT } from '@angular/core/src/render3/interfaces/view';
       let col = 0;
  
       this.colors.forEach(color => {
-        this.context.fillStyle = color;
-        this.context.fillRect(col, row, squareWidth, squareHeight); 
-        this.context.fillStyle = "#000";
-        this.context.rect(col, row, squareWidth, squareHeight); 
-        this.context.stroke();
+        this.colorsContext.fillStyle = color;
+        this.colorsContext.fillRect(col, row, squareWidth, squareHeight); 
+        this.colorsContext.fillStyle = "#000";
+        this.colorsContext.rect(col, row, squareWidth, squareHeight); 
+        this.colorsContext.stroke();
 
         col = col + squareWidth;
         if (col >= width) {
@@ -49,9 +52,13 @@ import { CONTEXT } from '@angular/core/src/render3/interfaces/view';
       let col = Math.floor(x / squareWidth);
       let row = Math.floor(y / squareHeight);
 
-      let pos = (5 * row) +col; 
-      console.log(this.colors[pos]);
+      let pos = (5 * row) +col;  
 
+      width =  this.paintingCanvas.nativeElement.width;
+      height = this.paintingCanvas.nativeElement.height;
+
+      this.paintingContext.fillStyle = this.colors[pos];
+      this.paintingContext.fillRect(0, 0, width, height);
 
     }
 
